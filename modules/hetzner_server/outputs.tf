ea42bea0a2fa14f9e61dc370c587647131c8f655
alessandrovitali <mail@alessandrovitali.net>
2026-09-03 01:15:01 +0200

    remove unreferenced outputs

--- modules/hetzner_server/outputs.tf
+++ modules/hetzner_server/outputs.tf
@@ -7,18 +7,3 @@ output "ipv6_address" {
   description = "Primary public IPv6 address of the host"
   value       = hcloud_server.this.ipv6_address
 }
-
-output "server_id" {
-  description = "Hetzner server ID"
-  value       = hcloud_server.this.id
-}
-
-output "ssh_key_name" {
-  description = "Name of the registered Hetzner SSH key"
-  value       = hcloud_ssh_key.this.name
-}
-
-output "firewall_name" {
-  description = "Name of the firewall attached to the host"
-  value       = hcloud_firewall.this.name
-}
